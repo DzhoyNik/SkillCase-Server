6a213056654bf31438fffd88380af4ec21794a86
DzhoyNik <DzhoyNik@yandex.ru>
2026-02-28 15:27:51 +0700

    feat(cors): add cors and setup express json

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const postgre = require('./database/postgre')
 const models = require('./models/index')
+const cors = require("cors")
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
