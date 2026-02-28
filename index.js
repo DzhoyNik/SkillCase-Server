16b798a5e35141e6a8ed1881e8de1e2fcdd7676c
DzhoyNik <DzhoyNik@yandex.ru>
2026-02-28 20:59:12 +0700

    feat: setup base router and api error

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const postgre = require('./database/postgre')
 const models = require('./models/index')
 const cors = require("cors")
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/v1', router)
 
 const start = async () => {
     try {
